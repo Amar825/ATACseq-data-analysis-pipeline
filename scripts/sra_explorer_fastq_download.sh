4ef3f8bf9fa9e13560fe636ba66252e2c2d0b7f3
Amar Khatri <61513322+Amar825@users.noreply.github.com>
2025-05-19 01:34:25 +0200

    Create sra_explorer_fastq_download.sh

---
+++ scripts/sra_explorer_fastq_download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/095/SRR10261595/SRR10261595_1.fastq.gz -o SRR10261595_ATAC-Seq_of_SC_x_SU_hybrid_at_30C_rep2_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/095/SRR10261595/SRR10261595_2.fastq.gz -o SRR10261595_ATAC-Seq_of_SC_x_SU_hybrid_at_30C_rep2_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/092/SRR10261592/SRR10261592_1.fastq.gz -o SRR10261592_ATAC-Seq_of_S._cerevisiae_at_30C_rep2_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/092/SRR10261592/SRR10261592_2.fastq.gz -o SRR10261592_ATAC-Seq_of_S._cerevisiae_at_30C_rep2_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/096/SRR10261596/SRR10261596_1.fastq.gz -o SRR10261596_ATAC-Seq_of_SC_x_SU_hybrid_at_30C_rep1_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/096/SRR10261596/SRR10261596_2.fastq.gz -o SRR10261596_ATAC-Seq_of_SC_x_SU_hybrid_at_30C_rep1_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/093/SRR10261593/SRR10261593_1.fastq.gz -o SRR10261593_ATAC-Seq_of_S._cerevisiae_at_30C_rep1_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/093/SRR10261593/SRR10261593_2.fastq.gz -o SRR10261593_ATAC-Seq_of_S._cerevisiae_at_30C_rep1_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/094/SRR10261594/SRR10261594_1.fastq.gz -o SRR10261594_ATAC-Seq_of_SC_x_SU_hybrid_at_30C_rep3_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/094/SRR10261594/SRR10261594_2.fastq.gz -o SRR10261594_ATAC-Seq_of_SC_x_SU_hybrid_at_30C_rep3_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/091/SRR10261591/SRR10261591_1.fastq.gz -o SRR10261591_ATAC-Seq_of_S._cerevisiae_at_30C_rep3_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR102/091/SRR10261591/SRR10261591_2.fastq.gz -o SRR10261591_ATAC-Seq_of_S._cerevisiae_at_30C_rep3_2.fastq.gz
